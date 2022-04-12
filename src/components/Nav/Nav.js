f5a724087bd6747dfdf002a6782de385ce8847c0
pjwhite08 <pamelajeanwhite@gmail.com>
2022-04-12 12:50:24 -0500

    hamburger working, but can see overflow

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -18,7 +18,35 @@ const Nav = () => {
   return (
     <nav className="nav-bar">
       <img src={logo} class="logo"/>
-      <div class="menu">
+    
+      <label>
+        <input type="checkbox"/>
+        <span class="menu">
+          <span class="hamburger"></span>
+        </span>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </label>
+    </nav>
+      
+  );
+};
+
+export default Nav;
+
+{/* THIS MENU WORKS- MINE */}
+{/* <nav className="nav-bar">
+<img src={logo} class="logo"/>
+       <div class="menu">
         <button 
           className="burger"
           onClick={displayMenu}
@@ -31,10 +59,5 @@ const Nav = () => {
           <Link to="/services" >Services</Link>
         </div>
       }
-      </div>
-      
-    </nav>
-  );
-};
-
-export default Nav;
+      </div> 
+      </nav> */}
